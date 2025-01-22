662df109d1d8b8c20581a25de0409bfde1b4ff41
Borig <borigthedwarf@gmail.com>
2025-01-22 20:49:30 +1300

    Auto-format with Biome

--- src/saves.ts
+++ src/saves.ts
@@ -2,29 +2,31 @@ import { join } from 'node:path';
 import { existsSync, readFileSync } from 'node:fs';
 import { logger } from '@runejs/common';
 
-
 interface PlayerSave {
     username: string;
     passwordHash: string;
 }
 
-export function loadPlayerSave(playerSaveDir: string, username: string): PlayerSave {
+export function loadPlayerSave(
+    playerSaveDir: string,
+    username: string,
+): PlayerSave {
     const fileName = `${username.toLowerCase()}.json`;
     const filePath = join(playerSaveDir, fileName);
 
-    if(!existsSync(filePath)) {
+    if (!existsSync(filePath)) {
         return null;
     }
 
     const fileData = readFileSync(filePath, 'utf8');
 
-    if(!fileData) {
+    if (!fileData) {
         return null;
     }
 
     try {
         return JSON.parse(fileData) as PlayerSave;
-    } catch(error) {
+    } catch (error) {
         logger.error(`Malformed player save data for ${username}.`);
         return null;
     }
